adc544d93256105ccc4ca1b99a6c42141481d7f7
Jaz wu <592015984b@gmail.com>
2019-07-15 17:05:28 +0800

    增加侧边栏，引入layer.js、jquery

--- view/nexmoe/layout.php
+++ view/nexmoe/layout.php
@@ -5,25 +5,109 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no"/>
 	<title><?php e($title.' - '.config('site_name'));?></title>
 	<link rel="stylesheet" href="//cdnjs.loli.net/ajax/libs/mdui/0.4.1/css/mdui.css">
-	<style>
-		body{background-color:#f2f5fa;padding-bottom:60px;background-position:center bottom;background-repeat:no-repeat;background-attachment:fixed}.nexmoe-item{margin:20px -8px 0!important;padding:15px!important;border-radius:5px;background-color:#fff;-webkit-box-shadow:0 .5em 3em rgba(161,177,204,.4);box-shadow:0 .5em 3em rgba(161,177,204,.4);background-color:#fff}.mdui-img-fluid,.mdui-video-fluid{border-radius:5px;border:1px solid #eee}.mdui-list{padding:0}.mdui-list-item{margin:0!important;border-radius:5px;padding:0 10px 0 5px!important;border:1px solid #eee;margin-bottom:10px!important}.mdui-list-item:last-child{margin-bottom:0!important}.mdui-list-item:first-child{border:none}.mdui-toolbar{width:auto;margin-top:60px!important}.mdui-appbar .mdui-toolbar{height:56px;font-size:16px}.mdui-toolbar>*{padding:0 6px;margin:0 2px;opacity:.5}.mdui-toolbar>.mdui-typo-headline{padding:0 16px 0 0}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.mdui-typo-headline,a.active{opacity:1}.mdui-container{max-width:980px}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:48px}.mdui-toolbar>a{padding:0 16px;line-height:30px;border-radius:30px;border:1px solid #eee}.mdui-toolbar>a:last-child{opacity:1;background-color:#1e89f2;color:#ffff}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}.mdui-toolbar>*{display:none}.mdui-toolbar>a:last-child,.mdui-toolbar>.mdui-typo-headline,.mdui-toolbar>i:first-child{display:block}}
-	</style>
 	<script src="//cdnjs.loli.net/ajax/libs/mdui/0.4.1/js/mdui.min.js"></script>
+	<script src="//cdn.bootcss.com/jquery/1.12.3/jquery.min.js"></script>	
+        <script src="https://cdn.staticfile.org/layer/2.3/layer.js"></script>
+	 <style>
+		.mdui-appbar .mdui-toolbar			
+			height:56px;
+			font-size: 10px;
+		}
+       		 .mdui-toolbar>*{
+			padding: 0 6px;
+			margin: 0 2px;
+			opacity:0.5;
+		}
+		.mdui-toolbar>.mdui-typo-headline{
+			padding: 0 1px 0 0;
+		}
+		.mdui-toolbar>i{
+			padding: 0;
+		}
+		.mdui-toolbar>a:hover,a.mdui-typo-headline,a.active{
+			opacity:1;
+		}
+		.mdui-container{
+			max-width:950px;
+		}
+		.mdui-list-item{
+			-webkit-transition:none;
+			transition:none;
+		}
+		.mdui-list>.th{
+			background-color:initial;
+		}
+		.mdui-list-item>a{
+			width:100%;
+			line-height: 48px
+		}
+		.mdui-list-item{
+			margin: 2px 0px;
+			padding:0;
+		}
+		.mdui-toolbar>a:last-child{
+			opacity:1;
+		}
+		#instantclick-bar {
+        		background: white;
+        	}
+		@media screen and (max-width:950px)	{		
+			.mdui-list-item .mdui-text-right{
+				display: none;
+			}
+			.mdui-container{
+				width:100% !important;
+				margin:0px;
+			}
+			.mdui-toolbar>*{				
+				display: none;
+			}
+			.mdui-toolbar>a:last-child,.mdui-toolbar>.mdui-typo-headline,.mdui-toolbar>i:first-child{
+				display: block;
+			}
+		}
+		.spec-col{padding:.9em;display:flex;align-items:center;white-space:nowrap;flex:1 50%;min-width:225px}
+		.spec-type{font-size:1.35em}
+		.spec-value{font-size:1.25em}
+		.spec-text{float:left}
+		.device-section{padding-top:30px}
+		.spec-device-img{height:auto;height:340px;padding-bottom:30px}
+		#dl-header{margin:0}
+		#dl-section{padding-top:10px}
+		#dl-latest{position:relative;top:50%;transform:translateY(-50%)}
+	</style>
 </head>
-<body class="mdui-theme-primary-blue-grey mdui-theme-accent-blue">
-	
-	<div class="mdui-container">
-	    <div class="mdui-container-fluid">
-	    <div class="mdui-toolbar nexmoe-item">
-			<a href="/"><?php e(config('site_name'));?></a>
-			<?php foreach((array)$navs as $n=>$l):?>
+<body class=" mdui-appbar-with-toolbar mdui-theme-primary-indigo mdui-theme-accent-pink">
+	<header class="mdui-appbar mdui-appbar-fixed mdui-color-theme mdui-appbar-inset">	
+	<div class="mdui-toolbar mdui-color-theme">
+        <span class="mdui-btn  mdui-typo-headline mdui-btn-icon mdui-ripple mdui-ripple-white" mdui-drawer="{target: '#main-drawer', swipe: true}" mdui-tooltip="{content: '菜单'}"><i class="mdui-icon material-icons">menu</i></span>
+		<a href="/" class="mdui-typo-headline"><?php e(config('site_name'));?></a>
+		<?php foreach((array)$navs as $n=>$l):?>
 			<i class="mdui-icon material-icons mdui-icon-dark" style="margin:0;">chevron_right</i>
 			<a href="<?php e($l);?>"><?php e($n);?></a>
-			<?php endforeach;?>
-			<!--<a href="javascript:;" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">refresh</i></a>-->
-		</div>
-		</div>
+		<?php endforeach;?>
+	</div>	
+	</header>
+
+<div class="mdui-drawer mdui-drawer-close mdui-color-indigo-50" id="main-drawer">
+	<div class="mdui-grid-tile">
+		<a href="javascript:;"><img src="//p.ssl.qhimg.com/t01ad8dba0b68972b26.jpg"/></a>
+		<div class="mdui-grid-tile-actions mdui-grid-tile-actions-gradient">
+			<div class="mdui-grid-tile-text">
+				<div class="mdui-grid-tile-title"><?php e($title.' - '.config('site_name'));?></div>
+				<div class="mdui-grid-tile-subtitle">Onedrive</div>
+    			</div>
+   		 </div>
+	</div>   
+	<div class="mdui-list" mdui-collapse="{accordion: true}">
+    		<?php e(config('drawer'));?>
+	</div>
+</div>
+  
+ 
+<div class="mdui-container">
+	<div class="mdui-container-fluid"></div>
     	<?php view::section('content');?>
-  	</div>
+</div>
 </body>
 </html>
